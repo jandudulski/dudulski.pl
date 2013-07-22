be9ed6d1a862813be72032f151f03db194d87c4b
Jan Dudulski <jan@dudulski.pl>
2013-07-22 15:36:33 +0200

    Plain old html in mina deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-require 'mina/bundler'
-require 'mina/rails'
+# require 'mina/bundler'
+# require 'mina/rails'
 require 'mina/git'
 # require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
 # require 'mina/rvm'    # for rvm support. (http://rvm.io)
@@ -10,7 +10,7 @@ require 'mina/git'
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :domain, 'cwaniak.eu'
+set :domain, 'cwaniak'
 set :deploy_to, '/var/www/dudulski.pl'
 set :repository, 'git://github.com/jandudulski/dudulski.pl.git'
 set :branch, 'master'
@@ -22,7 +22,7 @@ set :shared_paths, ['log', 'tmp']
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
 #   set :port, '30000'     # SSH port number.
-set :user, 'dudulski'
+set :user, 'deploy'
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
@@ -44,9 +44,6 @@ task :setup => :environment do
 
   queue! %[mkdir -p "#{deploy_to}/shared/config"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
-
-  queue! %[mkdir -p "#{deploy_to}/shared/tmp/pids"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/tmp/pids"]
 end
 
 desc "Deploys the current version to the server."
@@ -56,11 +53,6 @@ task :deploy => :environment do
     # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
-    invoke :'bundle:install'
-
-    to :launch do
-      queue 'touch tmp/restart.txt'
-    end
   end
 end
 
